8530d30522efd44456142c3518980fd572eaae31
lu-yousheng <2269975236@qq.com>
2023-07-21 13:35:52 +0800

    练习异常

--- java-20230717/src/Test.java
+++ java-20230717/src/Test.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) throws UserNameException{
+        System.out.println("请输入你的用户名！");
+        String username = "myz";
+        Scanner scanner = new Scanner(System.in);
+        try{
+            String name = scanner.nextLine();
+            if(!name.equals(username)){
+                throw new UserNameException();
+            }
+        }catch (UserNameException e){
+            e.printStackTrace();
+            System.out.println("用户名异常！");
+        }finally {
+            scanner.close();
+        }
+        //System.out.println("用户名成功！");
+    }
 }
---
+++ java-20230717/src/UserNameException.java
@@ -0,0 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class UserNameException extends Exception{
+    public UserNameException() {
+        super();
+    }
+}
